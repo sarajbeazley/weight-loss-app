49d2d224639b0d1cbd84b41b0101053b33832bc9
Sara Beazley <sarajbeazley@gmail.com>
2023-06-08 14:54:43 -0700

    added functionality to the ExpertForm

--- src/components/ExpertForm.js
+++ src/components/ExpertForm.js
@@ -1,8 +1,30 @@
-import { useLocation } from "react-router-dom";
-
+import { useState } from "react";
 
 export default function ExpertForm() {
-  
+  const [expertForm, setExpertForm] = useState({
+    firstName: " ",
+    lastName: " ",
+    emailAddress: " ",
+    phoneNumber: " ",
+    message: " ",
+  });
+
+  console.log(expertForm);
+
+  function handleChange(event) {
+    setExpertForm((prevExpertForm) => {
+      return {
+        ...prevExpertForm,
+        [event.target.name] : event.target.value
+      };
+    });
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    console.log(expertForm)
+  }
+
   return (
     <>
       <p className="expert">
@@ -13,12 +35,38 @@ export default function ExpertForm() {
         </p>
       </p>
       <container className="expert-form-container">
-        <form className="expert-form">
-          <input type="text" placeholder="First Name" />
-          <input type="text" placeholder="Last Name" />
-          <input type="email" placeholder="Email" />
-          <input type="phone" placeholder="Phone Number" />
-          <textarea type="message" placeholder="Message" />
+        <form className="expert-form" onSubmit={handleSubmit}>
+          <input
+            type="text"
+            placeholder="First Name"
+            onChange={handleChange}
+            name="firstName"
+            value={expertForm.firstName}
+          />
+          <input type="text" 
+          placeholder="Last Name" 
+          onChange={handleChange}
+          name="lastName"
+          value={expertForm.lastName}
+          />
+          <input type="email" 
+          placeholder="Email" 
+          onChange={handleChange}
+          name="emailAddress"
+          value={expertForm.email}
+          />
+          <input type="phone" 
+          placeholder="Phone Number" 
+          onChange={handleChange}
+          name="phoneNumber"
+          value={expertForm.phoneNumber}
+          />
+          <textarea type="message" 
+          placeholder="Message" 
+          onChange={handleChange}
+          name="message"
+          value={expertForm.message}
+          />
           <br></br>
           <button className="expert-submit">Submit</button>
         </form>
