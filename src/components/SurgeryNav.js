a95376e383bb393001682386b5c1dc3caa1a36fa
Amin Hasan <aminhasan@Amins-MacBook-Pro.local>
2023-06-15 14:54:07 +0100

    fixed dropdown and navbar

--- src/components/SurgeryNav.js
+++ src/components/SurgeryNav.js
@@ -1,27 +1,51 @@
-import { useState } from "react";
+import { useState, useEffect, useRef } from "react";
+import "../App.css";
 
-export default function SurgeryNav(){
-    const [isSurgeryOpen, setIsSurgeryOpen] = useState(false)
+export default function SurgeryNav() {
+  const [isSurgeryOpen, setIsSurgeryOpen] = useState(false);
+  const node = useRef();
 
-    const toggleSurgeryMenu = () =>{
-        setIsSurgeryOpen(!isSurgeryOpen);
+  useEffect(() => {
+    // this function will be called whenever a click event occurs
+    const handleClick = (e) => {
+      // if the target of the click is not within the dropdown or the Surgery menu item, close the dropdown
+      if (node.current.contains(e.target)) {
+        // inside click
+        return;
+      }
+      // outside click
+      setIsSurgeryOpen(false);
     };
 
-return (
-<nav>
-<ul>
-  <li className="surgery">
-    <p onClick={toggleSurgeryMenu}><b>Surgery</b></p>
-    {isSurgeryOpen && (
-      <ul className="dropdown-menu">
-        <li>Gastric band</li>
-        <li>Gastric bypass</li>
-        <li>Gastric sleeve</li>
-      </ul>
-    )}
-  </li>
-</ul>
-</nav>
-);
+    // attach the event listener to the window
+    window.addEventListener("click", handleClick);
 
+    // return function to be called when unmounted
+    return () => {
+      window.removeEventListener("click", handleClick);
+    };
+  }, []);
+
+  const toggleSurgeryMenu = () => {
+    setIsSurgeryOpen(!isSurgeryOpen);
+  };
+
+  return (
+    <nav>
+      <ul>
+        <li className="surgery" ref={node}>
+          <p onClick={toggleSurgeryMenu}>
+            <b>Surgery</b>
+          </p>
+          {isSurgeryOpen && (
+            <ul className="dropdown-menu show">
+              <li>Gastric band</li>
+              <li>Gastric bypass</li>
+              <li>Gastric sleeve</li>
+            </ul>
+          )}
+        </li>
+      </ul>
+    </nav>
+  );
 }
